0a2577c6fa97dc9aae8233add2a6cca69632757b
JonaSeguReymundo <00043823@uca.edu.sv>
2024-07-06 22:29:27 -0600

    Agregando comentarios a la clase DatabaseConection

--- Parcial Final/Codigo/src/main/java/codigo/codigo/DatabaseConection.java
+++ Parcial Final/Codigo/src/main/java/codigo/codigo/DatabaseConection.java
@@ -9,8 +9,8 @@ public class DatabaseConection { //00043823 Nombre de la case
     private static final String USER = "ParcialFinal"; //00043823 Hago el atributo estatico y final porque será el mismo USER
     private static final String PASSWORD = "parcialfinalpoo"; //00043823 Hago el atributo estatico y final porque será la misma PASSWORD
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() throws SQLException { //00043823 Agrego el método estatico de tipo Connection llamado getConnection
+        return DriverManager.getConnection(URL, USER, PASSWORD); //00043823 Retorna el método DriverManager.getConnection que tiene como parametro URL, USER, PASSWORD que son los antes declarados, ya aquí se hace la conexión
     }
 
 }
